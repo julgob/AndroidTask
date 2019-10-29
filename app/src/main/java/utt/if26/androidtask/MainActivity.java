f04cd0c5556c2840fad6c31ff6b08ae0e4cb76dd
jules gobin <jules.gobin@utt.fr>
2019-10-29 13:33:03 +0100

    added action bar to mainativity

--- app/src/main/java/utt/if26/androidtask/MainActivity.java
+++ app/src/main/java/utt/if26/androidtask/MainActivity.java
@@ -1,4 +1,16 @@
 package utt.if26.androidtask;
 
-public class MainActivity {
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
 }
